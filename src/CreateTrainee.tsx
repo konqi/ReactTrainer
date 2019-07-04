5074ae84b63eb871372842420891a1205c92a90e
Johann Wagner <johann.wagner@codecentric.de>
2019-07-04 17:45:24 +0200

    add new trainee mask

---
+++ src/CreateTrainee.tsx
@@ -0,0 +1,58 @@
+import { Button, TextField } from "@material-ui/core";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import * as React from "react";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    container: {
+      display: "grid",
+      gridTemplateRows: "auto auto auto",
+      justifyItems: "center"
+    },
+    textField: {
+      marginLeft: theme.spacing(1),
+      marginRight: theme.spacing(1),
+      width: 200
+    },
+    dense: {
+      marginTop: 19
+    },
+    menu: {
+      width: 200
+    }
+  })
+);
+
+export const CreateTrainee = () => {
+  const classes = useStyles();
+
+  return (
+    <form className={classes.container} noValidate autoComplete="off">
+      <TextField
+        id="standard-name"
+        label="Name"
+        className={classes.textField}
+        value={"Name"}
+        onChange={() => {}}
+        margin="normal"
+      />
+      <TextField
+        id="standard-name"
+        label="Preis"
+        className={classes.textField}
+        value={"Preis"}
+        onChange={() => {}}
+        margin="normal"
+      />
+      <Button
+        onClick={() => {
+          console.log("hello");
+        }}
+        variant="contained"
+        color="primary"
+      >
+        Anlegen
+      </Button>
+    </form>
+  );
+};
