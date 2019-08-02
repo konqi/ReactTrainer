b7f9767ae6cd1e0d975d4098ae30eb76b20bec00
Johann Wagner <johann.wagner@codecentric.de>
2019-08-02 15:19:03 +0200

    add tests for traineesagas

--- src/state/sagas/traineeSagas.spec.ts
+++ src/state/sagas/traineeSagas.spec.ts
@@ -6,7 +6,15 @@ import {
   deleteTrainee,
   fetchTrainee,
 } from './traineeSagas'
-import {TraineeActions} from '../trainees'
+import {TraineeActions, createAddTraineeAction} from '../trainees'
+import {
+  createSaveTraineeAction,
+  createDeleteTraineeAction,
+  createExpelTraineeAction,
+} from '../trainees/traineeActions'
+import {TraineeBuilder} from '../../__mocks__/store'
+import {dbInsertTrainee, dbDeleteTrainee} from '../../data/Queries'
+import {createExpelSessionAction} from '../sessions/sessionActions'
 
 test('trainee root saga', () => {
   const iterator = traineeSagas()
@@ -25,12 +33,77 @@ test('trainee root saga', () => {
 })
 
 describe('saveTrainee', () => {
-  test('missing payload', () => {})
-  test('successful save', () => {})
-  test('error during save', () => {})
+  test('successful save', () => {
+    const trainee = new TraineeBuilder().build()
+    const iterator = saveTrainee(createSaveTraineeAction(trainee))
+
+    // call db insert operation
+    let {value} = iterator.next()
+    expect(value).toEqual(call(dbInsertTrainee, trainee))
+
+    // add trainee to store
+    ;({value} = iterator.next({id: 'newTraineeId'}))
+    expect(value).toEqual(
+      put(createAddTraineeAction({...trainee, id: 'newTraineeId'}))
+    )
+
+    // should be done
+    const {done} = iterator.next()
+    expect(done).toBe(true)
+  })
+
+  test('missing payload', () => {
+    // @ts-ignore
+    const iterator = saveTrainee(createSaveTraineeAction())
+
+    const consoleErrorSpy = jest
+      .spyOn(console, 'error')
+      .mockImplementation(jest.fn())
+    const {done} = iterator.next()
+    expect(consoleErrorSpy).toHaveBeenCalled()
+    consoleErrorSpy.mockRestore()
+
+    expect(done).toBe(true)
+  })
+
+  test('error during save', () => {
+    const trainee = new TraineeBuilder().build()
+    const iterator = saveTrainee(createSaveTraineeAction(trainee))
+
+    // call db insert operation
+    let {value} = iterator.next()
+    expect(value).toEqual(call(dbInsertTrainee, trainee))
+
+    // add trainee to store
+    const consoleErrorSpy = jest
+        .spyOn(console, 'error')
+        .mockImplementation(jest.fn())
+
+      // fails because id is missing from result
+    ;({value} = iterator.next())
+    expect(consoleErrorSpy).toHaveBeenCalled()
+    consoleErrorSpy.mockRestore()
+
+    // should be done
+    const {done} = iterator.next()
+    expect(done).toBe(true)
+  })
 })
 describe('deleteTrainee', () => {
-  test('successful delete', () => {})
+  test('successful delete', () => {
+    const iterator = deleteTrainee(createDeleteTraineeAction('traineeId'))
+
+    // call db delete operation
+    let {value} = iterator.next()
+    expect(value).toEqual(call(dbDeleteTrainee, 'traineeId'))
+
+    // remove trainee from store
+    ;({value} = iterator.next())
+    expect(value).toEqual(put(createExpelTraineeAction('traineeId')))
+
+    const {done} = iterator.next()
+    expect(done).toBe(true)
+  })
 })
 describe('fetchTrainee', () => {
   test('missing parameters', () => {})
